8b0ed721ce0cfa30b12fc5f4f6ea7d46c28d2a65
Adam <adam.grossman08@me.com>
2014-10-14 16:30:42 -0700

    backup

--- classtracker/urls.py
+++ classtracker/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
 
     url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
+
     url(r'^admin/', include(admin.site.urls)),
 )
