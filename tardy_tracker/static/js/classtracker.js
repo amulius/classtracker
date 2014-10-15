fa4fef716c165425dbbd80b8e0fe3eca5d5bbb9d
amulius <joshua@Joshua-Stortzs-MacBook-Pro.local>
2014-10-14 23:33:43 -0700

    here is my version of the check in that removes the button after checking in

--- tardy_tracker/static/js/classtracker.js
+++ tardy_tracker/static/js/classtracker.js
@@ -5,18 +5,14 @@
 $(document).ready(function() {
 
     $('#checkIn').on('click', function () {
-        console.log("hi");
-        currentUser = $('#currentUser').data('username');
-        currentCourse = $('#currentCourse').data('course');
+        var currentUser = $('#currentUser').data('username');
+        var currentCourse = $('#currentCourse').data('course');
 
         var datas = {
             student: currentUser,
             course: currentCourse
         };
-
-        console.log(datas);
         datas = JSON.stringify(datas);
-
         $.ajax({
             url: '/checkin/',
             type: 'POST',
--- tardy_tracker/views.py
+++ tardy_tracker/views.py
@@ -28,11 +28,12 @@ def student_home(request):
     return render(request, 'student_home.html', data)
 
 
+
+
 def home(request):
     if request.user.is_student:
         current_time = datetime.datetime.now().time()
         today = datetime.datetime.now().date()
-        # courses = Course.objects.filter(students=request.user)
         checked_in = []
         courses = Course.objects.filter(students=request.user, start_time__lte=current_time, end_time__gte=current_time)
         if courses:
@@ -46,9 +47,14 @@ def home(request):
     else:
         current_time = datetime.datetime.now().time()
         print current_time
-        courses = Course.objects.filter(teacher=request.user, start_time__lte=current_time, end_time__gte=current_time)
+        current_course = Course.objects.filter(teacher=request.user, start_time__lte=current_time, end_time__gte=current_time)[0]
+        # current_status = {}
+        # for student in User.objects.filter(student_courses=current_course):
+
+        courses_today = Course.objects.filter(teacher=request.user)
         data = {
-            'courses': courses
+            'current_course': current_course,
+            'courses_today': courses_today
         }
         return render(request, 'teacher_home.html', data)
 
@@ -58,15 +64,9 @@ def new_check_in(request):
     print "check in"
     print request.method
     if request.method == 'POST':
-        # print request.body
-        # print request.body['student']
         data = json.loads(request.body)
         student = User.objects.get(username=data['student'])
-        print student
         course = Course.objects.get(name=data['course'])
-        print course
-        # check_in = CheckIn(course=course, student=student)
         check_in = CheckIn.objects.create(course=course, student=student)
-        print check_in
-    response = serializers.serialize('json', {check_in})
-    return HttpResponse(response, content_type='application/json')
+        response = serializers.serialize('json', {check_in})
+        return HttpResponse(response, content_type='application/json')
